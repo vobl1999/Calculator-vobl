58fb2cfa9d6342a2831488ae30fb372f9e6156ba
vobl1999 <maimaidx@hotmail.com>
2025-03-16 09:16:32 +0800

    Create functions.py

---
+++ functions/functions.py
@@ -0,0 +1,22 @@
+import math
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+def power(x, y):
+    return x ** y
+
+def sqrt(x):
+    return math.sqrt(x)
